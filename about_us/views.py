bf7834e850432221b6b3b0f1f77fbedde170cdba
Nayeem Hasan <nayeemtby@gmail.com>
2023-12-01 15:02:51 +0600

    Home done

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def aboutUs(req):
+    return render(req,'about_us/index.html')
--- web_ass3/urls.py
+++ web_ass3/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from about_us.views import aboutUs
+from meal.views import meals
+
+from web_ass3.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('meal', meals, name="meals"),
+    path('about', aboutUs, name="about")
 ]
